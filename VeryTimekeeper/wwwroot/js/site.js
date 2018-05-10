f2cf5e790bfd37d65e8360c2202f9e14dcd150ce
Kayla Ondracek <kayla.renee@gmail.com>
2018-05-10 15:41:32 -0700

    add first blush of a timer

--- VeryTimekeeper/wwwroot/js/site.js
+++ VeryTimekeeper/wwwroot/js/site.js
@@ -1,1 +1,11 @@
 ﻿// Write your Javascript code.
+var seconds_left = 10;
+var interval = setInterval(function () {
+    document.getElementById('timer1').innerHTML = --seconds_left;
+    document.title = seconds_left;
+
+    if (seconds_left <= 0) {
+        document.getElementById('timer1').innerHTML = 'task done';
+        clearInterval(interval);
+    }
+}, 1000);

9e62fdcba3b3f434956d3b27825c0a5f61b2f532
Kayla Ondracek <kayla.renee@gmail.com>
2018-05-10 13:50:26 -0700

    add database connection things, applicationuser model, dbcontext model

---
+++ VeryTimekeeper/Models/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace VeryTimekeeper.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Task> Tasks { get; set; }
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ VeryTimekeeper/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace VeryTimekeeper.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}

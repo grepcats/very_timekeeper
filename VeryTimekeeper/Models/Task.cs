510d6bba2f37863580d0542df68a9f064e141a4b
Kayla Ondracek <kayla.renee@gmail.com>
2018-05-10 13:46:43 -0700

    add task object and configure startup/csproj files

---
+++ VeryTimekeeper/Models/Task.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VeryTimekeeper.Models
+{
+    [Table("Tasks")]
+    public class Task
+    {
+        [Key]
+        public int TaskId { get; set; }
+        public string Content { get; set; }
+        public string timeTotal { get; set; }
+        public string timeRemaining { get; set; }
+        public DateTime timeToStart { get; set; }
+        public DateTime timeToFinish { get; set; }
+    }
+}
--- VeryTimekeeper/Startup.cs
+++ VeryTimekeeper/Startup.cs
@@ -1,51 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using VeryTimekeeper.Models;
+
+//Add this
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace VeryTimekeeper
 {
     public class Startup
     {
+        public IConfigurationRoot Configuration { get; set; }
         public Startup(IHostingEnvironment env)
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
+                .AddJsonFile("appsettings.json");
             Configuration = builder.Build();
         }
-
-        public IConfigurationRoot Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
             services.AddMvc();
+
+        //    services.AddEntityFrameworkMySql()
+        //      .AddDbContext<ApplicationDbContext>(options =>
+        //          options.UseMySql(Configuration["ConnectionStrings:DefaultConnection"])
+        //      );
+
+        //    services.AddIdentity<ApplicationUser, IdentityRole>()
+        //      .AddEntityFrameworkStores<ApplicationDbContext>()
+        //      .AddDefaultTokenProviders();
+
+
+            
+        //    services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+        //    {
+        //        options.Password.RequireUppercase = false;
+        //        options.Password.RequireNonAlphanumeric = false;
+        //        options.Password.RequiredLength = 0;
+        //        options.Password.RequireDigit = false;
+        //    });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
-
-            if (env.IsDevelopment())
-            {
-                app.UseBrowserLink();
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
+           
+            app.UseIdentity();
 
             app.UseStaticFiles();
 
@@ -54,6 +58,10 @@ namespace VeryTimekeeper
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+            });
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
             });
         }
     }

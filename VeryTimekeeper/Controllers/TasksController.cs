c3065509c2692bbd5103f66f529bd793813170b5
Kayla Ondracek <kayla.renee@gmail.com>
2018-05-10 15:06:00 -0700

    add tasks controller and create method

---
+++ VeryTimekeeper/Controllers/TasksController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using VeryTimekeeper.ViewModels;
+using VeryTimekeeper.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace VeryTimekeeper.Controllers
+{
+    public class TasksController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public TasksController(UserManager<ApplicationUser> userManager, ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Create(VeryTimekeeper.Models.Task task)
+        {
+            var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            _db.Tasks.Add(task);
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}

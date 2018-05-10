59b7f712a4762dd3b64444416bb4592c255cf02e
Kayla Ondracek <kayla.renee@gmail.com>
2018-05-10 16:12:36 -0700

    add timer to all tasks. not right behavior, but a start

--- VeryTimekeeper/wwwroot/js/site.js
+++ VeryTimekeeper/wwwroot/js/site.js
@@ -1,11 +1,33 @@
 ﻿// Write your Javascript code.
-var seconds_left = 10;
-var interval = setInterval(function () {
-    document.getElementById('timer1').innerHTML = --seconds_left;
-    document.title = seconds_left;
 
-    if (seconds_left <= 0) {
-        document.getElementById('timer1').innerHTML = 'task done';
-        clearInterval(interval);
-    }
-}, 1000);
+function startTimer() {
+    console.log(allTasks);
+    //var seconds_left = 10;
+    //var interval = setInterval(function () {
+
+    //    document.getElementById('timer1').innerHTML = --seconds_left;
+    //    document.title = seconds_left;
+
+    //    if (seconds_left <= 0) {
+    //        document.getElementById('timer1').innerHTML = 'task done';
+    //        clearInterval(interval);
+    //    }
+    //}, 1000);
+
+    allTasks.forEach(function (task) {
+        var seconds_left = 10;
+        var interval = setInterval(function () {
+
+            document.getElementById('timer' + task.taskId).innerHTML = --seconds_left;
+            document.title = seconds_left;
+
+            if (seconds_left <= 0) {
+                document.getElementById('timer' + task.taskId).innerHTML = 'task done';
+                clearInterval(interval);
+            }
+        }, 1000);
+    })
+ 
+
+
+}

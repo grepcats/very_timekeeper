d29d7d3576a3338ba1b30e57080431946377864b
Kayla Ondracek <kayla.renee@gmail.com>
2018-05-14 08:47:36 -0700

    format hours, minutes, seconds in the tab

--- VeryTimekeeper/wwwroot/js/site.js
+++ VeryTimekeeper/wwwroot/js/site.js
@@ -2,24 +2,33 @@
 
 function startTimer() {
     console.log(allTasks);
-    //var seconds_left = 10;
-    //var interval = setInterval(function () {
-
-    //    document.getElementById('timer1').innerHTML = --seconds_left;
-    //    document.title = seconds_left;
-
-    //    if (seconds_left <= 0) {
-    //        document.getElementById('timer1').innerHTML = 'task done';
-    //        clearInterval(interval);
-    //    }
-    //}, 1000);
 
     allTasks.forEach(function (task) {
-        var seconds_left = 10;
+        var seconds_left = 3601;
+        
+
         var interval = setInterval(function () {
+            let min = 0;
+            let sec = 0;
+            let hr = 0;
 
             document.getElementById('timer' + task.taskId).innerHTML = --seconds_left;
-            document.title = seconds_left;
+            if (seconds_left >= 3600) {
+                hr = Math.floor(seconds_left / 3600)
+                min = seconds_left % 60;
+                if (min >= 60) {
+                    min = Math.floor(seconds_left / 60);
+                    sec = seconds_left % 60;
+                }
+            } else if (seconds_left >= 60) {
+                min = Math.floor(seconds_left / 60);
+                sec = seconds_left % 60;
+            }
+            else {
+                sec = seconds_left;
+            }
+            console.log(hr + ":" + min + ":" + sec)
+            document.title = hr + ":" + min + ":" + sec;
 
             if (seconds_left <= 0) {
                 document.getElementById('timer' + task.taskId).innerHTML = 'task done';

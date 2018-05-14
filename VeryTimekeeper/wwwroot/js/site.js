7aca375662024c72346fd05a441f8f3c73046054
Kayla Ondracek <kayla.renee@gmail.com>
2018-05-14 10:38:36 -0700

    run task timer one task at a time and move to next task when complete

--- VeryTimekeeper/wwwroot/js/site.js
+++ VeryTimekeeper/wwwroot/js/site.js
@@ -1,42 +1,51 @@
 ﻿// Write your Javascript code.
 
 function startTimer() {
-    console.log(allTasks);
+    let task = allTasks[0];
+    let seconds_left = task.timeRemaining;
+    var interval = setInterval(function () {
+        let min = 0;
+        let sec = 0;
+        let hr = 0;
 
-    allTasks.forEach(function (task) {
-        var seconds_left = task.timeRemaining;
-        
-
-        var interval = setInterval(function () {
-            let min = 0;
-            let sec = 0;
-            let hr = 0;
-
-            document.getElementById('timer' + task.taskId).innerHTML = --seconds_left;
-            if (seconds_left >= 3600) {
-                hr = Math.floor(seconds_left / 3600)
-                min = seconds_left % 60;
-                if (min >= 60) {
-                    min = Math.floor(seconds_left / 60);
-                    sec = seconds_left % 60;
-                }
-            } else if (seconds_left >= 60) {
+        document.getElementById('timer' + task.taskId).innerHTML = --seconds_left;
+        if (seconds_left >= 3600) {
+            hr = Math.floor(seconds_left / 3600)
+            min = seconds_left % 60;
+            if (min >= 60) {
                 min = Math.floor(seconds_left / 60);
                 sec = seconds_left % 60;
             }
-            else {
-                sec = seconds_left;
-            }
-            console.log(hr + ":" + min + ":" + sec)
-            document.title = hr + ":" + min + ":" + sec;
+        } else if (seconds_left >= 60) {
+            min = Math.floor(seconds_left / 60);
+            sec = seconds_left % 60;
+        }
+        else {
+            sec = seconds_left;
+        }
+        console.log(hr + ":" + min + ":" + sec)
+        document.title = hr + ":" + min + ":" + sec;
 
-            if (seconds_left <= 0) {
-                document.getElementById('timer' + task.taskId).innerHTML = 'task done';
-                clearInterval(interval);
+        if (seconds_left <= 0) {
+            document.getElementById('timer' + task.taskId).innerHTML = 'task done';
+            let completeItem = allTasks.shift();
+            clearInterval(interval);
+            document.title = 'task done';
+            if (allTasks.length > 0) {
+                startTimer();
             }
-        }, 1000);
-    })
+            
+        }
+    }, 1000);
 }
+
+function moveToNext() {
+
+}
+        
+        
+
+       
 
 //settimer (on model)
 //starttimer

2950624d3acc63ecbd2aac8e6ae6c894db9deaa4
Kayla Ondracek <kayla.renee@gmail.com>
2018-05-14 10:11:42 -0700

    use create form to take in and conver time remaining

--- VeryTimekeeper/Controllers/TasksController.cs
+++ VeryTimekeeper/Controllers/TasksController.cs
@@ -37,6 +37,38 @@ namespace VeryTimekeeper.Controllers
         {
             var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
             task.User = await _userManager.FindByIdAsync(userId);
+            int hr = 0;
+            int min = 0;
+            int sec = 0;
+            if (String.IsNullOrEmpty(Request.Form["hours"]))
+            {
+                hr = 0;
+            }
+            else
+            {
+                hr = Int32.Parse(Request.Form["hours"]);
+            }
+
+            if (String.IsNullOrEmpty(Request.Form["minutes"]))
+            {
+                min = 0;
+            }
+            else
+            {
+                min = Int32.Parse(Request.Form["minutes"]);
+            }
+
+            if (String.IsNullOrEmpty(Request.Form["seconds"]))
+            {
+                sec = 0;
+            }
+            else
+            {
+                sec = Int32.Parse(Request.Form["seconds"]);
+            }
+
+        
+            task.timeRemaining = (hr * 3600) + (min * 60) + sec;
             _db.Tasks.Add(task);
             await _db.SaveChangesAsync();
             return RedirectToAction("Index");
